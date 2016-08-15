1b5a1a119d73aac7898611bdc2569813e455975e
linux_ch <chenh0829@foxmail.com>
2016-08-15 16:57:28 +0800

    编译c代码为.so文件 add jna 封装调用.so文件

--- src/main/java/com/itgary/pro/jna/ItestJna.java
+++ src/main/java/com/itgary/pro/jna/ItestJna.java
@@ -8,7 +8,8 @@ import com.sun.jna.Native;
  */
 public interface ItestJna extends Library {
 
-    //gcc -fPIC -shared -g -o liba.so a.c
+    //gcc -fPIC -shared -g -o liba.so a.c  (编写动态库.so文件)
+    //gcc -o test_1 test1.c                (编写.c文件.可执行文件)
     ItestJna INSTANCE = (ItestJna) Native.loadLibrary("linux-x86-64/a.so",ItestJna.class);
     int print_str();
 }
