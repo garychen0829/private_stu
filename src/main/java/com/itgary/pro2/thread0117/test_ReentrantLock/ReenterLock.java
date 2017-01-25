a6be423ef5b8a3dab3d1fb83037b390dd600a571
ch96 <ch96@zcsmart.com>
2017-01-25 10:04:52 +0800

    IO 和 多线程

---
+++ src/main/java/com/itgary/pro2/thread0117/test_ReentrantLock/ReenterLock.java
@@ -0,0 +1,39 @@
+package com.itgary.pro2.thread0117.test_ReentrantLock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 冲入锁
+ * Created by garychen on 2017/1/24.
+ */
+public class ReenterLock implements Runnable{
+
+    public static ReentrantLock lock = new ReentrantLock();
+    public static int i = 0;
+
+
+    @Override
+    public  void run() {
+        for (int j = 0; j < 1000; j++) {
+            lock.lock();
+            try {
+                i++;
+
+                System.out.println(Thread.currentThread().getName() + "在执行. [i] : " + i);
+            }finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ReenterLock reenterLock = new ReenterLock();
+        Thread t1 = new Thread(reenterLock);
+        Thread t2 = new Thread(reenterLock);
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println("out : "+i);
+    }
+}
---
+++ src/main/java/com/itgary/pro2/thread0117/test_Samaphore/Pool.java
@@ -0,0 +1,14 @@
+package com.itgary.pro2.thread0117.test_Samaphore;
+
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * Created by garychen on 2017/1/24.
+ */
+public class Pool {
+
+
+
+
+}
