bb35d1b4f355991f851ab9aa127e8763eebd5f82
ch96 <ch96@zcsmart.com>
2017-03-14 09:53:48 +0800

    callBack-回调函数.

---
+++ src/main/java/com/itgary/pro/callb/CallBackInterface.java
@@ -0,0 +1,11 @@
+package com.itgary.pro.callb;
+
+/**
+ * Created by garychen on 2017/3/13.
+ */
+public interface CallBackInterface {
+    public void doSome();
+
+    public void executeMethod();
+
+}
